04264ee2d933650905a134ca8f0dc5f88a0dc8dd
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-11-14 17:49:59 -0800

    deployed

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # build
-npm run build
+yarn build
 
 # navigate into the build output directory
 cd dist
@@ -12,10 +12,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:pilgrimmemoirs.github.io.git master
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pilgrimmemoirs/split.git master:gh-pages
+git push -f git@github.com:pilgrimmemoirs/Split.git master:gh-pages
 
 cd -
